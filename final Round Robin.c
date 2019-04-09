f083dd6133f5812b873a4adcb4696f39534761a1
VShubham20 <47859561+VShubham20@users.noreply.github.com>
2019-04-09 22:11:43 +0530

    Add files via upload

---
+++ final Round Robin.c
@@ -0,0 +1,145 @@
+/*
+ Name - Shubham Verma
+ Section - K17SM
+ Roll no - B41
+ Reg. no - 11716628
+ */
+
+#include<stdio.h> 
+
+struct process                        // structure for process
+{
+	char name;
+	int at,bt,wt,tt,rt;
+	int completed;
+}p[10];
+
+int n;
+int q[10];  //queue
+int front = -1, rear = -1;
+
+void enqueue(int i)                     // for inserting process
+{
+    if(rear==10)
+        printf("overflow");
+    rear++;
+    q[rear]=i;
+    if(front==-1)
+        front=0;
+
+}
+
+int dequeue()                     // for deleting process after completion
+{
+    if(front==-1)
+        printf("underflow");
+    int temp=q[front];
+    if(front==rear)
+        front=rear=-1;
+    else
+        front++;
+    return temp;
+}
+int isInQueue(int i)
+{
+    int k;
+    for(k=front;k<=rear;k++)
+    {
+        if(q[k]==i)
+        return 1;
+    }
+    return 0;
+}
+
+void sortByArrival()                    // sorting process
+{
+    struct process temp;
+    int i,j;
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(p[i].at>p[j].at)
+            {
+                temp=p[i];
+                p[i]=p[j];
+                p[j]=temp;
+            }
+        }
+    }
+}
+
+void main()
+{
+    int i,j,time=0,sum_bt=0,tq;
+    char c;
+    float avgwt=0;
+     printf("\nEnter Number of Processes:\n");
+     scanf("%d",&n);
+     for(i=0; i < n; i++)
+     {
+         p[i].name=c;
+         printf("\n Process %d\n",i+1);
+         printf(" Arrival Time :");
+         scanf("%d",&p[i].at);
+         printf(" Burst Time :");
+         scanf("%d",&p[i].bt);
+         p[i].rt=p[i].bt;
+         p[i].completed=0;
+         sum_bt+=p[i].bt;
+    }
+    printf("\nEnter the time quantum:");
+    scanf("%d",&tq);
+    sortByArrival();
+    enqueue(0);          // enqueue the first process
+    printf("Process execution order: ");
+    for(time=p[0].at;time<sum_bt;)       // run until the total burst time reached
+    {   
+        i=dequeue();
+        if(p[i].rt<=tq)
+        {                          /* for processes having remaining time with less than                                             or  equal  to time quantum  */
+                              
+            time+=p[i].rt;
+            p[i].rt=0;
+            p[i].completed=1;         
+            printf(" %c ",p[i].name);
+            p[i].wt=time-p[i].at-p[i].bt;
+            p[i].tt=time-p[i].at;      
+            for(j=0;j<n;j++)     /*enqueue the processes which have come while scheduling */
+            {
+                if(p[j].at<=time && p[j].completed!=1&& isInQueue(j)!=1)
+                {
+                    enqueue(j);
+                }
+            }
+        }
+        else               // more than time quantum
+        {
+            time+=tq;
+            p[i].rt-=tq;
+            printf(" %c ",p[i].name);
+            for(j=0;j<n;j++)    /*first enqueue the processes which have come while scheduling */
+            {
+                if(p[j].at<=time && p[j].completed!=1&&i!=j&& isInQueue(j)!=1)
+                {
+                    enqueue(j);
+                }
+            }
+            enqueue(i);   // then enqueue the uncompleted process
+        }
+    }
+
+    printf("\nName\tArrival Time\tBurst Time\tWaiting Time\tTurnAround Time");
+    for(i=0;i<n;i++)
+    {
+        avgwt+=p[i].wt;
+        printf("\nP%d\t\t%d\t\t%d\t\t%d\t\t%d",i+1,p[i].at,p[i].bt,p[i].wt,p[i].tt);
+    }
+    float avgtt;
+    for(i = 0; i < n; i++)
+    {
+    	avgtt+=p[i].tt;
+	}
+    printf("\nAverage waiting time:%f\n",avgwt/n);
+    printf("\nAverage turnaround time:%f\n",avgtt/n);
+}
---
+++ final round robin with priority.cpp
@@ -0,0 +1,234 @@
+/*
+ Name - Shubham Verma
+ Section - K17SM
+ Roll no - B41
+ Reg. no - 11716628
+ */
+#include <iostream>
+#include<stdio.h>
+using namespace std;
+
+struct process
+{
+    int arr,burst,prior,firstt,i,status,rect,burl,compt,waitingt,turnaroundt;
+}*run=NULL;
+
+int n,tq,tburst=0;
+struct node
+{
+    struct process *q;
+    struct node *next;
+}*start=NULL,*last,*cn;
+
+int main()
+{
+    printf("\nEnter total number of the processes you want to execute :");
+    scanf("%d",&n);
+    int pl = n;                               //  processes remaining
+    printf("Enter the value of time quantum :\n");
+    scanf("%d",&tq);                        //  taking value of time quantum
+    struct process p[n];           
+    for(int i = 0; i < n; i++)              // taking details of every process
+    {
+        printf("\nEnter the details of P %d\n", i+1);        
+        printf("\tArrival Time : ");                 // taking arrival time
+        scanf("%d", &p[i].arr);
+        printf("\tBurst Time : ");                    // taking burst time
+        scanf("%d", &p[i].burst);
+        p[i].burl = p[i].burst;
+        printf("\tPriority : ");
+        scanf("%d", &p[i].prior);
+        p[i].i = i+1;
+        p[i].status = 0;
+        p[i].rect = -1; 
+        p[i].firstt = -1;
+    }
+    printf("\n\n Table entered by the user is as following:");;
+    printf("\n _______________________________________________________________________________________________");
+    printf("\n|\tPROCESS\t\t|\tARRIVAL TIME\t|\tBURST TIME\t|\tPRIORITY\t|");
+    printf("\n|_______________________|_______________________|_______________________|_______________________|");
+    for(int i=0;i<n;i++)
+    {
+        printf("\n|\tP%d\t\t|\t%d\t\t|\t%d\t\t|\t%d\t\t|",p[i].i,p[i].arr,p[i].burst,p[i].prior);
+    }
+    printf("\n|_______________________________________________________________________________________________|");
+    for(int time = 0; pl != 0; time++)
+    {
+        for(int i = 0; i < n; i++)                                                                    //priority increment
+        {
+            if(p[i].rect == -1 && p[i].status == 1)
+            {
+                if((time)%10 == (p[i].arr)%10)
+                {
+                    p[i].prior++;
+                    printf("\nPriority of process P%d increasing by 1.", p[i].i);
+                    node *x;
+                    x = start;
+                    while(x->next->q->i==i)
+                        x = x->next;
+                    cn = new node;
+                    cn->q = x->next->q;
+                    cn->next = NULL;
+                    x->next = x->next->next;
+                    if(start == NULL)
+                    {
+                        last = cn;
+                        start = cn;
+                    }
+                    else{
+                        node *x;
+                        x = start;
+                        if((start->q->prior)<(cn->q->prior))
+                        {
+                            cn->next = start;
+                            start = cn;
+                        }
+                        else
+                        {
+                            while(x->next!=NULL && x->next->q->prior > cn->q->prior)
+                            {
+                                x = x->next;
+                            }
+                            cn->next = x->next;
+                            x->next = cn;
+                        }
+                    }
+                }
+            }
+        }
+        for(int i = 0; i < n; i++)
+        {
+            if(p[i].arr == time && p[i].status == 0)                                                //in the queue
+            {
+                if(start == NULL)
+                {
+                    p[i].status = 1;
+                    cn = new node;
+                    cn->q = &p[i];
+                    cn->next = NULL;
+                    last = cn;
+                    start = cn;
+                }
+                else{
+                    p[i].status = 1;
+                    cn = new node;
+                    cn->q = &p[i];
+                    node *x;
+                    x = start;
+                    if((start->q->prior)<(cn->q->prior))
+                    {
+                        cn->next = start;
+                        start = cn;
+                    }
+                    else
+                    {
+                        while(x->next != NULL && x->next->q->prior > cn->q->prior)
+                        {
+                            x = x->next;
+                        }
+                        cn->next = x->next;
+                        x->next = cn;
+                    }
+                }
+            }
+        }
+    qwerty:
+        if(run == NULL)   //running of process
+        {
+            if(start != NULL)
+            {
+                run = start->q;
+                start = start->next;
+                if(run->firstt == -1)
+                {
+                    run->firstt = time;
+                }
+                run->rect = time;
+            }
+        }
+        if(run != NULL && (time)<((run->rect)+tq) && run->status != 2)
+        {
+            if(start != NULL)
+            {
+                if(run->prior < start->q->prior &&  start->q->arr == time)             //if higher priority process com in existence  then context switching happens
+                {
+                    printf("\nContext switching happening as priority of process P%d is greater than priority of process P%d",start->q->i,run->i);       
+                    cn = new node;
+                    cn->q = run;
+                    cn->next = NULL;
+                    node *x;
+                    x = start;
+                    while(x->next != NULL && x->next->q->prior > cn->q->prior)
+                    {
+                        x = x->next;
+                    }
+                    cn->next = x->next;
+                    x->next = cn;
+                    run = NULL;
+                    goto qwerty;
+                }
+            }
+            run->burl--;
+            printf("\n process %d running at time %d",run->i,time);
+            if(run->burl == 0)                                                    //when process got completed
+            {
+                run->status = 2;
+                pl--;
+                run->compt = (time+1);
+                run->turnaroundt = ((run->compt)-(run->arr));
+                run->waitingt = ((run->turnaroundt)-(run->burst));
+                run=NULL;
+            }
+            else if((time+1)==((run->rect)+tq))
+            {
+                cn = new node;
+                cn->q = run;
+                cn->next = NULL;
+                if(start == NULL)
+                {
+                    last = cn;
+                    start = cn;
+                }
+                else
+                {
+                    node *x;
+                    x = start;
+                    if((start->q->prior)<(cn->q->prior))
+                    {
+                        cn->next = start;
+                        start = cn;
+                    }
+                    else
+                    {
+                        while(x->next != NULL && x->next->q->prior >= cn->q->prior)
+                        {
+                            x = x->next;
+                        }
+                        cn->next = x->next;
+                        x->next = cn;
+                    }
+                    run = NULL;
+                }
+            }
+        }
+    }
+    printf("\n\n\nTable after scheduling  :");
+    printf("\n ___________________________________________________________________________________________________________________________________________");
+    printf("\n|  PROCESS\t|\tARRIVAL TIME\t|\tBURST TIME\t|\tPRIORITY\t|\tWAITING TIME  \t|\tTURN AROUND TIME    |");
+    printf("\n|_______________|_______________________|_______________________|_______________________|_______________________|___________________________|\n");
+    for(int i = 0; i < n; i++)
+    {
+        printf("|\tP%d\t|\t%d\t\t|\t%d\t\t|\t%d\t\t|\t%d\t\t|\t%d\t\t    |\n",p[i].i,p[i].arr,p[i].burst,p[i].prior,p[i].waitingt,p[i].turnaroundt);
+    }
+    printf("\n|___________________________________________________________________________________________________________________________________________|");
+    float awt=0,att=0;
+    for(int i=0;i<n;i++)
+    {
+        awt = awt+p[i].waitingt;
+        att = att+p[i].turnaroundt;
+    }
+    awt = awt/n;
+    att = att/n;
+    printf("\nAverage  waiting time = %f ",awt);
+    printf("\nAverage Turn Around Time = %f \n",att);
+}
